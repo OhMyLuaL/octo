3cdaca8d1abfa0f99cbdbfbe9143e9f56e7b5348
Ivan Lialiuk <lualgod92@gmail.com>
2022-10-29 12:57:51 +0300

    route and routing

--- app.js
+++ app.js
@@ -3,7 +3,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.port || 4444;
+const test = require("./routes/test");
+
+app.use(express.json());
+app.use("/test", test);
 
 app.get("/", (req, res) => {
 
+});
+
+app.listen(port, err => {
+    if(err){
+        return console.log("ERROR", err);
+    }
+    console.log(`listening port on ${port}`);
 });
