c26fffe45649667404e858f98e82e6bafc981daa
Ivan Lialiuk <lualgod92@gmail.com>
2022-11-10 18:58:56 +0300

    new changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.port || 4444;
-const test = require("./routes/test");
+const test = require("./routes/authRoute");
 
 app.use(express.json());
 app.use("/test", test);
